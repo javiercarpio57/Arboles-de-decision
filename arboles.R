182727a03991dabca81a8bff0b5c2f792a7145f4
paulbelches <paulbelches@gmail.com>
2020-03-09 16:42:33 -0600

    Numerical filter

--- arboles.R
+++ arboles.R
@@ -1,4 +1,7 @@
-getwd()
+library(dplyr) #Para usar select
+
 setwd("~/Documents/Mineria/Arboles-de-decision")
 houses <- read.csv("train.csv")
-houses
+housesFilter <-select(houses, LotFrontage, LotArea, YearBuilt, YearRemodAdd, MasVnrArea, BsmtFinSF1,BsmtFinSF2,BsmtUnfSF,TotalBsmtSF,X1stFlrSF,X2ndFlrSF,LowQualFinSF,GrLivArea,TotRmsAbvGrd,Fireplaces,GarageYrBlt,GarageCars,GarageArea,WoodDeckSF,OpenPorchSF,EnclosedPorch,ScreenPorch,PoolArea,MoSold,YrSold)
+plot(housesFilter[1,])
+
